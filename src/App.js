6b1df8f8d1980edae998706fe9c1c034665fdd7b
cricketbackground <vadzz.java@gmail.com>
2020-11-12 13:50:07 -0500

    updated app name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Config, CloudConfigOptions, ConfigObject } from 'spring-cloud-config';
+
+const cloudConfigOptions: CloudConfigOptions = {
+  configPath: __dirname + '/config',
+  activeProfiles: ['dev1'],
+  level: 'debug'
+};
+
+let myConfig: ConfigObject;
+
+Config.load(cloudConfigOptions).then((theConfig: ConfigObject) => {
+  myConfig = theConfig;
+  // now run your application with the loaded config props.
+  // do this by saving the returned config object somewhere,
+  // or by using the Config.instance() helper.
+});
 
 function App() {
+  //console.log(`Employee Phone is : ${Config.instance().employee.phone}`);
   return (
     <div className="App">
       <header className="App-header">
